5ae0a70620f4001e20f08e7cb3ddbe36c9368144
EgorPotolov <97224725+EgorPotolov@users.noreply.github.com>
2022-08-28 14:28:12 +0300

    Update add.py

--- add.py
+++ add.py
@@ -3,36 +3,6 @@ import
 
 bot = telebot.TeleBot(TOKEN)
 
-class ConvertionException(Exception):
-    pass
-
-
-class CryptoConverter:
-    @staticmethod
-    def convert(quote: str, base: str, amount: str):
-        if quote == base:
-            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
-
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {quote}')
-
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {base}')
-
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConvertionException(f'Не удалось обработать количество {amount}')
-
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
-
-        return total_base
-
     @bot.message_handler(commands=['start', 'help'])
     def handle_start_help(message: telebot.types.Message):
         text = 'Что бы начать работу введите комманду боту в следующем формате: \n<имя валюты> \
